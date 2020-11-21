7535547f213c6c323061a7a497ae227319f9000b
Nusendra Hanggarawan <admin@nusendra.com>
2020-11-21 20:17:48 +0700

    feat: created login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -8,19 +8,23 @@ import Register from "./components/Register";
 const App = () => {
   const user = null;
 
-  return user ? (
-    <Home />
-  ) : (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-      </Switch>
-    </Router>
+  return (
+    <div className="limiter">
+      {user ? (
+        <Home />
+      ) : (
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
+      )}
+    </div>
   );
 };
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,66 @@
-const login = () => {
-  return <div>login</div>;
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginWithEmailPassword = () => {
+    console.log("aw");
+  };
+
+  return (
+    <div className="container-login100">
+      <div className="wrap-login100 p-t-50 p-b-90">
+        <form className="login100-form validate-form flex-sb flex-w">
+          <span className="login100-form-title p-b-51">Login</span>
+
+          <div
+            className="wrap-input100 validate-input m-b-16"
+            data-validate="Username is required"
+          >
+            <input
+              className="input100"
+              type="text"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <span className="focus-input100"></span>
+          </div>
+
+          <div
+            className="wrap-input100 validate-input m-b-16"
+            data-validate="Password is required"
+          >
+            <input
+              className="input100"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span className="focus-input100"></span>
+          </div>
+
+          <div className="flex-sb-m w-full p-t-3 p-b-24">
+            <div>
+              <Link to="/tes" className="txt1">
+                Register
+              </Link>
+            </div>
+          </div>
+
+          <div className="container-login100-form-btn m-t-17">
+            <button
+              className="login100-form-btn"
+              onClick={loginWithEmailPassword}
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default login;
+export default Login;
